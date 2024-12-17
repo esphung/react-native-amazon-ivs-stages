d0d2211109fd3ac0f7cadee47fbda10cdaa9db73
Eric Phung <esphung@gmail.com>
2024-12-16 20:22:44 -0600

    build: updated package json scripts and clean script

--- .scripts/clean-project.sh
+++ .scripts/clean-project.sh
@@ -11,5 +11,16 @@ rm -rf ./ios/DerivedData/
 rm -rf ./Gemfile.lock
 rm -rf ./yarn.lock
 yarn install
-(cd ios && rm -rf ./Pods/ ./Podfile.lock && pod install --repo-update)
-(cd android && ./gradlew clean)
+
+# clean example project
+rm -rf ./node_modules/
+rm -rf ./ios/build/
+rm -rf ./ios/DerivedData/
+rm -rf ./Gemfile.lock
+rm -rf ./yarn.lock
+(cd android && rm -rf ./app/.cxx/)
+yarn install
+(cd ios && rm -rf ./Pods/ ./Podfile.lock)
+(cd ios && RCT_NEW_ARCH_ENABLED=1 pod install --repo-update)
+(cd android && ORG_GRADLE_PROJECT_newArchEnabled=true ./gradlew clean)
+(cd android && ORG_GRADLE_PROJECT_newArchEnabled=true ./gradlew wrapper)
